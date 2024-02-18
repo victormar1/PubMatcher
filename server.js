eafa34f729277f1003ce37331a12efe03f63ac44
victormar1 <victor.marin@chu-bordeaux.fr>
2024-02-18 14:33:21 +0100

    v0.2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const https = require('https');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -8,9 +9,12 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const XLSX = require('xlsx');
+const { get } = require('http');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 // Load the databases into memory
 const mouseDBPath = path.join(__dirname, 'BDD', 'Mouse.xlsx');
@@ -26,6 +30,28 @@ app.get('/', (req, res) => {
 });
 
 app.post('/search', async (req, res) => {
+    const results = await getData(req);
+    res.render('index', { results, phenotypes: req.body.phenotypes });
+});
+
+
+app.get('/api/search', async (req, res) => {
+    const genesQuery = req.query.genes ? req.query.genes.split(',') : [];
+    const phenotypesQuery = req.query.phenotypes ? req.query.phenotypes.split(',') : [];
+
+    req.body = req.query
+    const results = await getData(req);
+
+    res.json({
+        query: {
+            genes: genesQuery,
+            phenotypes: phenotypesQuery
+        },
+        results: results
+    });
+});
+
+async function getData(req) {
     const genes = req.body.genes.split(',').map(gene => gene.trim()).filter(item => item !== "N/A")
     const genesWithoutDuplicate = [...new Set(genes)];
     const phenotypes = req.body.phenotypes.split(',').map(phenotype => phenotype.trim());
@@ -36,6 +62,7 @@ app.post('/search', async (req, res) => {
         const queries = phenotypes.map(phenotype => `(${gene} AND ${phenotype})`);
         const combinedQuery = queries.join(' OR ');
         const url = `https://pubmed.ncbi.nlm.nih.gov/?term=${encodeURIComponent(combinedQuery)}`;
+        const uniprotapi = `https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&exact_gene=${gene}&organism=homo%20sapiens`
             
         try {
             const response = await axios.get(url);
@@ -52,15 +79,38 @@ app.post('/search', async (req, res) => {
 
             // Match with the databases
             const mouseMatch = mouseData.find(row => row.Gene === gene);
-            const proteinMatch = proteinData.find(row => row.Gene === gene);
-            let mousePhenotype
 
+
+            //Version Get Uniprot
+
+            const responseuniprot = await axios.get(uniprotapi);
+            let proteinMatch = "";
+            if (responseuniprot.status === 200) {
+                await responseuniprot.data.some(function (element){
+                    if(element.comments){
+                        const comments = element.comments
+                        const finded = comments.some(function(comment) {
+                            if (comment.type === 'FUNCTION') {
+                                proteinMatch = comment.text[0].value;
+                                return true;
+                            }
+                        })
+                        if (finded) return true
+                    }
+                }); 
+            }
+
+           
+            // Version DB Excel en
+            //const proteinMatch = proteinData.find(row => row.Gene === gene);
+
+            let mousePhenotype
             if (path.includes("term")) {
                 results.push({
                     gene,
                     title: title || "No result",
                     count: isNaN(count) ? 0 : count,
-                    function: proteinMatch ? proteinMatch.annotation : "No match",
+                    function: proteinMatch !== "" ? proteinMatch : "No match",
                     mousePhenotype: (mouseMatch && !(typeof mouseMatch['Souris KO'] === "undefined")) ? mouseMatch['Souris KO'] : "No match",
                     url: url
                 });
@@ -79,13 +129,34 @@ app.post('/search', async (req, res) => {
         }
     }
 
-res.render('index', { results, phenotypes: req.body.phenotypes });
 
+    // Récupération des données PanelApp pour chaque gène
+    for (let i = 0; i < results.length; i++) {
+        try {
+            const panelAppResponse = await axios.get(`https://panelapp.genomicsengland.co.uk/api/v1/genes/?entity_name=${results[i].gene}&format=json`);
+            results[i].panelApp = panelAppResponse.data.count;
+        } catch (error) {
+            console.error(`Error fetching PanelApp data for gene ${results[i].gene}: `, error);
+            results[i].panelApp = 'Error';
+        }
+    }
 
-});
+    return results;
+}
 
-const PORT = 8080;
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Chemins vers votre certificat SSL et votre clé privée
+const privateKeyPath = '/etc/letsencrypt/live/pubmatcher.fr/privkey.pem';
+const certificatePath = '/etc/letsencrypt/live/pubmatcher.fr/fullchain.pem';
+
+// Lire les fichiers de certificat
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// Créer un serveur HTTPS avec les certificats
+const httpsServer = https.createServer(credentials, app);
+
+const PORT = 443;
+httpsServer.listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
 });
