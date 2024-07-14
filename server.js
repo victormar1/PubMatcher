b080b5f6be44588a1f0908ce4f0d19e6d7c723d5
victormar1 <156513930+victormar1@users.noreply.github.com>
2024-07-14 23:49:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 
 // Configuration de nodemailer
