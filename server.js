5203a893de0a98e24dc22d22871de103e99ac0af
victormar1 <156513930+victormar1@users.noreply.github.com>
2024-07-14 23:42:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const PdfPrinter = require('pdfmake');
 const nodemailer = require('nodemailer');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
