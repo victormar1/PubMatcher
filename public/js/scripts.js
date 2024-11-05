55613d11793b8dd1611a7e27bae71347192e4cf8
victormar1 <156513930+victormar1@users.noreply.github.com>
2024-11-05 11:45:09 +0100

    Victormar1 beta5 (#6)
    * Add files via upload
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js order
    
    * Update server.js
    
    * Update server.jsretry
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update genes.csv
    
    added RNU7-190P
    
    * Update server.js
    
    * Add files via upload
    
    * Update index.ejs
    
    * Update server.js
    
    * Update server.js
    
    * Update index.ejs
    
    Added Bug Report
    
    * Update index.ejs
    
    * Update index.ejs
    
    * Update server.js
    
    added bug report
    
    * Update server.js
    
    Test Promise.all approach
    
    * Update server.js
    
    fixed single result
    
    * Update server.js
    
    fixed
    
    * Update index.ejs
    
    Added analytics
    
    * Update index.ejs
    
    Loading screen
    
    * Update index.ejs
    
    retest
    
    * Update index.ejs
    
    reverted to ganalytics point
    
    * Create styles.css
    
    * Update styles.css
    
    * Create scripts.js
    
    * Update index.ejs
    
    * Create results.ejs
    
    * Update server.js
    
    * Update index.ejs
    
    * Update server.js
    
    * Create 1
    
    * Update results.ejs
    
    * Add files via upload
    
    * Update server.js
    
    * Update results.ejs
    
    * Update and rename constraints.csv to BDD/constraints.csv
    
    * Update results.ejs
    
    * Update results.ejs
    
    * Update server.js
    
    * Add files via upload
    
    Added API get

---
+++ public/js/scripts.js
@@ -0,0 +1,105 @@
+// scripts.js
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-PGNGTTRTN2');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('bug-report-modal');
+    const btn = document.getElementById('bug-report-button');
+    const span = document.getElementsByClassName('close')[0];
+    const searchForm = document.getElementById('searchForm');
+    const genesInput = document.getElementById('genesInput');
+    const seqOneBtn = document.getElementById('seqOneBtn');
+    const seqOnePopin = document.getElementById('seqOnePopin');
+    const seqOneInput = document.getElementById('seqOneInput');
+    const seqOneSubmit = document.getElementById('seqOneSubmit');
+
+    btn.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+
+    document.getElementById('bug-report-form').onsubmit = async function(event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const message = document.getElementById('message').value;
+        console.log('Sending bug report:', { name, message });
+        try {
+            const response = await fetch('/reportbug', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name, message })
+            });
+            if (response.ok) {
+                alert('Bug report sent successfully');
+                modal.style.display = 'none';
+            } else {
+                const errorText = await response.text();
+                console.error('Failed to send bug report:', errorText);
+                alert('Failed to send bug report');
+            }
+        } catch (error) {
+            console.error('Network error:', error);
+            alert('Network error');
+        }
+    }
+
+    document.getElementById('textFreeInputBtn').addEventListener('click', () => {
+        document.getElementById('textFreePopin').style.display = 'block';
+    });
+
+    document.getElementById('textFreeSubmit').addEventListener('click', () => {
+        const text = document.getElementById('textFreeInput').value;
+        fetch('/extract-genes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: text })
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('genesInput').value = data.genes.join(', ');
+            document.getElementById('textFreePopin').style.display = 'none';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    });
+
+    seqOneBtn.addEventListener('click', () => {
+        seqOnePopin.style.display = 'block';
+    });
+
+    seqOneSubmit.addEventListener('click', () => {
+        const text = seqOneInput.value;
+        const lines = text.split('\n');
+        let genes = [];
+        if (lines.some(line => line.startsWith('New') || /^[1-5]$/.test(line.trim()))) {
+            lines.forEach((line, index) => {
+                if (line.startsWith('New') && lines[index + 1]) {
+                    genes.push(lines[index + 1].split('\t')[0]);
+                }
+            });
+        } 
+        genesInput.value = genes.join(', ');
+        seqOnePopin.style.display = 'none';
+    });
+
+    if (document.getElementById('resultsTable')) {
+        document.getElementById("resultsTable").scrollIntoView({ behavior: 'smooth' });
+    }
+});
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 
 const express = require('express');
 const https = require('https');
@@ -10,20 +11,79 @@ const path = require('path');
 const app = express();
 const XLSX = require('xlsx');
 const { get } = require('http');
+const moment = require('moment');
+const PdfPrinter = require('pdfmake');
+const nodemailer = require('nodemailer');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
+
+// Configuration de nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+// Route pour rapporter un bug
+app.post('/reportbug', (req, res) => {
+    console.log('Received /reportbug request'); // Ajout de message de débogage
+
+    const { name, message } = req.body;
+    console.log('Bug report data:', { name, message }); // Ajout de message de débogage
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.ADMIN_EMAIL,
+        subject: 'Bug Report from Pubmatcher',
+        text: `Name: ${name}\n\nMessage: ${message}`
+    };
+
+    console.log('Mail Options:', mailOptions); // Ajout de message de débogage
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent:', info.response);
+            res.status(200).send('Email sent');
+        }
+    });
+    res.status(200).send("Bug report received");
+});
+
+console.log('Bug report route configured.');
 
 // Load the databases into memory
 const mouseDBPath = path.join(__dirname, 'BDD', 'Mouse.xlsx');
 const proteinDBPath = path.join(__dirname, 'BDD', 'Protein.xlsx');
+const constraints = {};
 
 const mouseDB = XLSX.readFile(mouseDBPath);
 const proteinDB = XLSX.readFile(proteinDBPath);
 const mouseData = XLSX.utils.sheet_to_json(mouseDB.Sheets[mouseDB.SheetNames[0]]);
 const proteinData = XLSX.utils.sheet_to_json(proteinDB.Sheets[proteinDB.SheetNames[0]]);
+
+fs.createReadStream(path.join(__dirname, 'BDD', 'constraints.csv'))
+  .pipe(csv({ separator: ';' })) // Specify the correct delimiter if needed
+  .on('data', (row) => {
+    // Ensure the gene names match those in your results
+    constraints[row.gene] = {
+      pLI: row.pLI,
+      oe_mis_upper: row.oe_mis_upper,
+      oe_lof_upper: row.oe_lof_upper,
+      mis_z: row.mis_z
+    };
+  })
+  .on('end', () => {
+    console.log('Constraints CSV file successfully processed');
+  });
 
 app.get('/', (req, res) => {
     res.render('index', { results: [], phenotypes: '' });
@@ -34,6 +94,11 @@ app.post('/search', async (req, res) => {
     res.render('index', { results, phenotypes: req.body.phenotypes });
 });
 
+app.get('/search', async (req, res) => {
+    req.body = req.query
+    const results = await getData(req);
+    res.render('index', { results, phenotypes: req.body.phenotypes });
+});
 
 app.get('/api/search', async (req, res) => {
     const genesQuery = req.query.genes ? req.query.genes.split(',') : [];
@@ -52,24 +117,19 @@ app.get('/api/search', async (req, res) => {
 });
 
 async function getData(req) {
-    const genes = req.body.genes.split(',').map(gene => gene.trim()).filter(item => item !== "N/A")
-    const genesWithoutDuplicate = [...new Set(genes)];
+    const genes = req.body.genes.split(',').map(gene => gene.trim()).filter(item => item !== "N/A");
+    const genesWithoutDuplicate = Array.from(new Set(genes));
     const phenotypes = req.body.phenotypes.split(',').map(phenotype => phenotype.trim());
-    
-    let results = [];
-    
-    for (let gene of genesWithoutDuplicate) {
+
+    const results = await Promise.all(genesWithoutDuplicate.map(async (gene) => {
         const queries = phenotypes.map(phenotype => `(${gene} AND ${phenotype})`);
         const combinedQuery = queries.join(' OR ');
         const url = `https://pubmed.ncbi.nlm.nih.gov/?term=${encodeURIComponent(combinedQuery)}`;
-        const uniprotapi = `https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&exact_gene=${gene}&organism=homo%20sapiens`
-            
+        const uniprotapi = `https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&exact_gene=${gene}&organism=homo%20sapiens`;
+
         try {
-            const response = await axios.get(url);
-            const respReq = response.request
-            const path = respReq['path']
+            const [response, responseuniprot] = await Promise.all([axios.get(url), axios.get(uniprotapi)]);
             const $ = cheerio.load(response.data);
-                
             const titleSelector = "#search-results > section > div.search-results-chunks > div > article:nth-child(2) > div.docsum-wrap > div.docsum-content > a";
             const countSelector = "#search-results > div.top-wrapper > div.results-amount-container > div.results-amount > h3 > span";
 
@@ -77,72 +137,202 @@ async function getData(req) {
             const countText = $(countSelector).text().trim().replace(',', '');
             const count = parseInt(countText, 10);
 
-            // Match with the databases
-            const mouseMatch = mouseData.find(row => row.Gene === gene);
-
-
-            //Version Get Uniprot
-
-            const responseuniprot = await axios.get(uniprotapi);
             let proteinMatch = "";
             if (responseuniprot.status === 200) {
-                await responseuniprot.data.some(function (element){
-                    if(element.comments){
-                        const comments = element.comments
-                        const finded = comments.some(function(comment) {
+                await responseuniprot.data.some((element) => {
+                    if (element.comments) {
+                        const comments = element.comments;
+                        const finded = comments.some((comment) => {
                             if (comment.type === 'FUNCTION') {
                                 proteinMatch = comment.text[0].value;
                                 return true;
                             }
-                        })
-                        if (finded) return true
+                        });
+                        if (finded) return true;
                     }
-                }); 
+                });
             }
 
-           
-            // Version DB Excel en
-            //const proteinMatch = proteinData.find(row => row.Gene === gene);
+            if (proteinMatch.length > 800) {
+                proteinMatch = proteinMatch.substring(0, 800) + " [...]";
+            }
 
-            let mousePhenotype
-            if (path.includes("term")) {
-                results.push({
+            const mouseMatch = mouseData.find(row => row.Gene === gene);
+            const path = response.request.path;
+
+                       if (!path.includes("term")) {
+                const result = {
+                    gene,
+                    title: $("#full-view-heading > h1.heading-title").text().trim(),
+                    count: 1,
+                    function: proteinMatch ? proteinMatch : "No match",
+                    mousePhenotype: (mouseMatch && !(typeof mouseMatch['Souris KO'] === "undefined")) ? mouseMatch['Souris KO'] : "No match",
+                    url: url,
+                    panelAppEnglandCount: 0,  // default to 0 until populated
+                    panelAppAustraliaCount: 0  // default to 0 until populated
+                };
+
+                // Ajouter les contraintes pour ce gène
+                if (constraints[gene]) {
+                    result.constraints = constraints[gene];
+                } else {
+                    result.constraints = { pLI: 'N/A', oe_mis_upper: 'N/A', oe_lof_upper: 'N/A', mis_z: 'N/A' };
+                }
+
+                return result;
+            } else {
+                const result = {
                     gene,
                     title: title || "No result",
                     count: isNaN(count) ? 0 : count,
                     function: proteinMatch !== "" ? proteinMatch : "No match",
                     mousePhenotype: (mouseMatch && !(typeof mouseMatch['Souris KO'] === "undefined")) ? mouseMatch['Souris KO'] : "No match",
-                    url: url
-                });
-            } else {
-                results.push({
-                    gene,
-                    title: $("#full-view-heading > h1.heading-title").text().trim(),
-                    count: 1,
-                    function: proteinMatch ? proteinMatch.annotation : "No match",
-                    mousePhenotype: (mouseMatch && !(typeof mouseMatch['Souris KO'] === "undefined")) ? mouseMatch['Souris KO'] : "No match",
-                    url: url
-                });
+                    url: url,
+                    panelAppEnglandCount: 0,  // default to 0 until populated
+                    panelAppAustraliaCount: 0  // default to 0 until populated
+                };
+
+                // Ajouter les contraintes pour ce gène
+                if (constraints[gene]) {
+                    result.constraints = constraints[gene];
+                } else {
+                    result.constraints = { pLI: 'N/A', oe_mis_upper: 'N/A', oe_lof_upper: 'N/A', mis_z: 'N/A' };
+                }
+
+                return result;
             }
         } catch (error) {
             console.error(`Erreur lors de la recherche pour ${combinedQuery}:`, error);
+            return null;
         }
-    }
+    }));
 
-
-    // Récupération des données PanelApp pour chaque gène
-    for (let i = 0; i < results.length; i++) {
+    const validResults = results.filter(result => result !== null);
+    
+    await Promise.all(validResults.map(async (result) => {
         try {
-            const panelAppResponse = await axios.get(`https://panelapp.genomicsengland.co.uk/api/v1/genes/?entity_name=${results[i].gene}&format=json`);
-            results[i].panelApp = panelAppResponse.data.count;
+            const [panelAppEnglandResponse, panelAppAustraliaResponse] = await Promise.all([
+                axios.get(`https://panelapp.genomicsengland.co.uk/api/v1/genes/?entity_name=${result.gene}&format=json`),
+                axios.get(`https://panelapp.agha.umccr.org/api/v1/genes/?entity_name=${result.gene}&format=json`)
+            ]);
+
+            result.panelAppEnglandCount = panelAppEnglandResponse.data.count;
+            result.panelAppAustraliaCount = panelAppAustraliaResponse.data.count;
         } catch (error) {
-            console.error(`Error fetching PanelApp data for gene ${results[i].gene}: `, error);
-            results[i].panelApp = 'Error';
+            console.error(`Error fetching PanelApp data for gene ${result.gene}: `, error);
+            result.panelAppEnglandCount = 'Error';
+            result.panelAppAustraliaCount = 'Error';
         }
+    }));
+
+    return validResults;
+}
+
+
+
+// Route pour générer le PDF
+app.post('/export-pdf', (req, res) => {
+    const results = JSON.parse(req.body.results);
+    const phenotypes = req.body.phenotypes;
+
+    // Polices par défaut pour pdfmake
+    const fonts = {
+        Roboto: {
+            normal: 'Helvetica',
+            bold: 'Helvetica-Bold',
+            italics: 'Helvetica-Oblique',
+            bolditalics: 'Helvetica-BoldOblique'
+        }
+    };
+
+    // Fonction pour créer une cellule avec une image ou du texte
+    function createCell(content, fontSize = 8) {
+        if (content === "No match") {
+            return { image: path.join(__dirname, 'public', 'cross.png'), width: 10, alignment: 'center' };
+        }
+        return { text: content, alignment: 'center', fontSize, margin: [0, 5] };
     }
 
-    return results;
-}
+    // Ajustement des largeurs des colonnes
+    const colWidths = [50, 90, 30, 140, 100, 40]; // Réduction de moitié de la colonne PanelApp ENG/AUS et augmentation de Function
+
+    // Contenu du document PDF
+    const docDefinition = {
+        pageSize: 'A4',
+        pageOrientation: 'portrait',
+        pageMargins: [20, 20, 20, 40], // Réduction des marges en haut
+        content: [
+            {
+                columns: [
+                    { image: path.join(__dirname, 'public', 'logo.png'), width: 50 }, // Taille du logo réduite
+                    {
+                        width: '*',
+                        stack: [
+                            { text: 'Search Results', style: 'header', alignment: 'center' },
+                            { text: `Generated on: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`, style: 'subheader', alignment: 'center' },
+                            { text: 'Phenotypes:', style: 'subheader', alignment: 'center' },
+                            { text: phenotypes, alignment: 'center', margin: [0, 5, 0, 0] } // Marge ajustée
+                        ]
+                    }
+                ]
+            },
+            {
+                style: 'tableExample',
+                table: {
+                    headerRows: 1,
+                    widths: colWidths,
+                    body: [
+                        ['Gene', 'Title', 'Count', 'Function', 'Mouse Phenotype', 'PanelApp\nENG/AUS'].map(header => ({ text: header, fontSize: 8, bold: true, alignment: 'center' })), // Retour à la ligne
+                        ...results.map(result => [
+                            createCell(result.gene),
+                            createCell(result.title && result.title.length > 800 ? result.title.substring(0, 800) + '...' : result.title || "No result"),
+                            createCell(result.count ? result.count.toString() : "0"),
+                            createCell(result.function && result.function.length > 800 ? result.function.substring(0, 800) + '...' : result.function || "No match"),
+                            createCell(result.mousePhenotype && result.mousePhenotype.length > 800 ? result.mousePhenotype.substring(0, 800) + '...' : result.mousePhenotype || "No match"),
+                            createCell(`${result.panelAppEnglandCount.toString()}/${result.panelAppAustraliaCount.toString()}`)
+                        ])
+                    ]
+                },
+                layout: 'lightHorizontalLines'
+            }
+        ],
+        styles: {
+            header: {
+                fontSize: 18, // Police ajustée
+                bold: true,
+                margin: [0, 5, 0, 5] // Réduction de la marge en haut et en bas
+            },
+            subheader: {
+                fontSize: 12, // Police ajustée
+                bold: true,
+                margin: [0, 5, 0, 5] // Réduction de la marge en haut et en bas
+            },
+            tableExample: {
+                margin: [0, 5, 0, 15] // Réduction de la marge
+            }
+        },
+        footer: function (currentPage, pageCount) {
+            return {
+                columns: [
+                    { text: '', alignment: 'left' },
+                    { image: path.join(__dirname, 'public', 'logo.png'), width: 20, alignment: 'right' } // Petit logo en bas à droite
+                ],
+                margin: [20, 0]
+            };
+        }
+    };
+
+    // Génération du PDF
+    const printer = new PdfPrinter(fonts);
+    const pdfDoc = printer.createPdfKitDocument(docDefinition);
+    const filename = `PM_${moment().format('DDMMYY_HHmm')}.pdf`;
+    res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+    res.setHeader('Content-type', 'application/pdf');
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+});
+
+
 
 // Chemins vers votre certificat SSL et votre clé privée
 const privateKeyPath = '/etc/letsencrypt/live/pubmatcher.fr/privkey.pem';
@@ -161,6 +351,7 @@ httpsServer.listen(PORT, () => {
     console.log(`Server is running on https://localhost:${PORT}`);
 });
 
+
 let genesList = []; // Pour stocker les gènes du fichier CSV
 
 // Fonction pour lire et stocker les gènes du fichier CSV
@@ -168,7 +359,7 @@ function loadGenesFromFile(filePath) {
     fs.createReadStream(filePath)
       .pipe(csv())
       .on('data', (row) => {
-        genesList.push(row.geneName); 
+        genesList.push(row.geneName); // Assurez-vous que 'geneName' correspond à la clé de votre fichier CSV
       })
       .on('end', () => {
         console.log('CSV file successfully processed');
@@ -178,20 +369,14 @@ function loadGenesFromFile(filePath) {
 // Appel de la fonction pour charger les gènes
 loadGenesFromFile('./BDD/genes.csv'); // Remplacez par le chemin correct de votre fichier CSV
 
-// Route pour traiter le texte libre et extraire les gènes
 app.post('/extract-genes', (req, res) => {
     const text = req.body.text;
-    // Diviser le texte en mots en utilisant un séparateur standard.
-    const words = text.split(/\W+/); // Cette regex divise par tout ce qui n'est pas un mot.
-    
-    let foundGenes = [];
-    // Parcourir chaque mot du texte
-    words.forEach(word => {
-        // Vérifier si le mot correspond exactement à un gène dans la liste
-        if (genesList.includes(word)) {
-            foundGenes.push(word); // Ajouter le gène à la liste s'il correspond
-        }
+    const foundGenes = genesList.filter(gene => {
+        const regex = new RegExp(`\\b${gene}\\b`, 'i'); // correspondance exacte insensible à la casse
+        return regex.test(text);
     });
-
+    // Trier les gènes trouvés selon leur ordre d'apparition dans le texte
+    foundGenes.sort((a, b) => text.indexOf(a) - text.indexOf(b));
     res.json({ genes: foundGenes });
 });
+
