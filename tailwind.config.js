33491aad4240abcef7004ea07f48b5bcbf02c5f5
HugoLannes <hugo.lannes@gmail.com>
2024-11-13 12:44:02 +0100

    feat: homepage design

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{html,ejs,js}", // Add your EJS templates here
+    "./public/**/*.{html,js}", // Add your HTML and JavaScript files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
