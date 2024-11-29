bdd5acd2c70b556bf1b2dc063def2e5670c27a2a
HugoLannes <hugo.lannes@gmail.com>
2024-11-29 11:49:26 +0100

    fix: pubmatcher app title

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -8,7 +8,7 @@
                 <form class="space-y-4 md:space-y-6" action="#" @submit.prevent="login">
                     <div>
                         <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                        <input v-model="formData.username" type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MarinVic" required="">
+                        <input v-model="formData.username"  type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required="">
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
