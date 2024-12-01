dfa6a10accfcf517533f0a7f37ab2d5d916b2fb1
HugoLannes <hugo.lannes@gmail.com>
2024-12-01 20:51:04 +0100

    fix : dark

--- src/app.vue
+++ src/app.vue
@@ -10,36 +10,33 @@
 </template>
 
 <script>
-  import AppHeader from './components/AppHeader.vue';
-  import AppFooter from './components/AppFooter.vue';
-  import PatchNote from './components/PatchNote.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+import PatchNote from './components/PatchNote.vue';
 
-  export default {
-    name: 'App',
-    components: {
-      AppHeader,
-      AppFooter,
-      PatchNote,
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+    PatchNote,
+  },
+  mounted() {
+    this.applyDarkModePreference();
+  },
+  methods: {
+    applyDarkModePreference() {
+      const storedTheme = localStorage.getItem('color-theme');
+
+      if (storedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
     },
-    mounted() {
-      this.applyDarkModePreference();
-    },
-    methods: {
-      applyDarkModePreference() {
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches; //os theme remove if conflicting
-        const storedTheme = localStorage.getItem('color-theme');
-
-        if (storedTheme === 'dark' || (!storedTheme && prefersDark)) {
-          document.documentElement.classList.add('dark');
-        } else {
-          document.documentElement.classList.remove('dark');
-        }
-      },
-    }
-  };
+  }
+};
 
 
-  
+
 </script>
-
-
