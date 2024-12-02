833f08412394517782b264d1d03549ade0274bac
HugoLannes <hugo.lannes@gmail.com>
2024-12-02 14:37:50 +0100

    fix : better banner

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
         <div class="flex items-center mx-auto">
             <p class="flex items-center text-sm font-normal text-white dark:text-gray-400">
                 <span
-                    class="inline-flex p-1 me-3 bg-white rounded-full dark:bg-gray-600 w-6 h-6 items-center justify-center flex-shrink-0">
+                    class="inline-flex p-1 me-3 bg-white rounded-full dark:bg-gray-600 size-4 items-center justify-center flex-shrink-0">
                     <svg class="w-3 h-3 text-red-500 dark:text-gray-400" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
                         <path
@@ -12,21 +12,17 @@
                     </svg>
                     <span class="sr-only">Light bulb</span>
                 </span>
-                <span>{{ bannerMessage }}</span>
+                <span>Scheduled Maintenance: <a class="font-semibold">Monday, December 2</a> from <a
+                        class="font-semibold">23:00</a>
+                    to <a class="font-semibold">00:00</a>. Services
+                    may be
+                    temporarily
+                    unavailable
+                    during this time. Thank you for your understanding.</span>
 
             </p>
         </div>
-        <div class="flex items-center">
-            <button @click="closeBanner" type="button"
-                class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-white hover:bg-gray-200 hover:text-gray-900 rounded-full text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span class="sr-only">Close banner</span>
-            </button>
-        </div>
+
     </div>
 </template>
 
@@ -36,23 +32,9 @@
 export default {
     data() {
         return {
-            bannerVisible: true,
-            bannerMessage: "Scheduled Maintenance: 10/11/2023 from 23:00 to 00:00. Services may be temporarily unavailable. Thank you for your understanding." // ! Change in nano to display the banner DONT FORGET TO UPDATE THE MESSAGE
-        }
+            bannerVisible: true, // ACTIVATE BANNER 
+        };
     },
-    mounted() {
-        if (localStorage.getItem('bannerClosed') === this.bannerMessage) {
-            this.bannerVisible = false;
-        } else {
-            this.bannerVisible = true;
-        }
-    },
-    methods: {
-        closeBanner() {
-            localStorage.setItem('bannerClosed', this.bannerMessage);
-            this.bannerVisible = false;
-        },
-    }
 }
 
 </script>
