e7c7b09e4f41634e18b242a9d4b775dc0e6a0d8f
HugoLannes <hugo.lannes@gmail.com>
2025-04-20 00:31:41 +0200

    feat: add Docker configuration with Dockerfile, docker-compose, and .dockerignore

--- config/server.js
+++ config/server.js
@@ -1,31 +1,16 @@
 // config/server.js
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
+const http = require('http')
 
 function configureServer(app) {
-    const mode = process.env.NODE_ENV || 'development';
-    console.log(`Server is starting in ${mode} mode...`);
-    const isProduction = process.env.NODE_ENV === 'production';
+  const port = process.env.PORT || 3000
+  const mode = process.env.NODE_ENV || 'development'
 
-    if (isProduction) {
-        const privateKeyPath = '/etc/letsencrypt/live/pubmatcher.fr/privkey.pem';
-        const certificatePath = '/etc/letsencrypt/live/pubmatcher.fr/fullchain.pem';
+  console.log(`Server is starting in ${mode} mode...`)
 
-        const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
-        const certificate = fs.readFileSync(certificatePath, 'utf8');
-        const credentials = { key: privateKey, cert: certificate };
-
-        const httpsServer = https.createServer(credentials, app);
-        httpsServer.listen(443, () => {
-            console.log(`Running on https://localhost:443`);
-        });
-    } else {
-        const httpServer = http.createServer(app);
-        httpServer.listen(3000, () => {
-            console.log('Running on http://localhost:3000');
-        });
-    }
+  const server = http.createServer(app)
+  server.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`)
+  })
 }
 
-module.exports = { configureServer };
+module.exports = { configureServer }
